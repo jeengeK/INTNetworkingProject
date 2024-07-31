d3d9cd772613da7e5d2daf7d2e2b4a6ce7b1ffcc
jeengeK <ibensabo@gmail.com>
2024-07-31 12:33:40 +0300

    script test

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -6,9 +6,9 @@ if [ -z "$KEY_PATH" ]; then
     exit 5
 fi
 
-# Check for the correct number of arguments
+# Ensure the correct number of arguments is provided
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 <public-instance-ip> [<private-instance-ip> <command>]"
+    echo "Usage: $0 <public-instance-ip> [<private-instance-ip> [<command>]]"
     echo "Please provide bastion IP address"
     exit 5
 fi
@@ -17,21 +17,19 @@ fi
 PUBLIC_INSTANCE_IP="$1"
 PRIVATE_INSTANCE_IP="$2"
 COMMAND="$3"
-#nothing
 
 # Function to connect to the public instance directly
 connect_to_public_instance() {
-    ssh -i "$KEY_PATH" "ubuntu@$16.171.235.220"
+    ssh -i "$KEY_PATH" "ubuntu@$PUBLIC_INSTANCE_IP"
 }
 
 # Function to connect to the private instance through the public instance
 connect_to_private_instance() {
-    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_INSTANCE_IP" ubuntu@$PRIVATE_INSTANCE_IP
-}
-
-# Function to execute a command on the private instance
-execute_command_on_private_instance() {
-    ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_INSTANCE_IP" ubuntu@$PRIVATE_INSTANCE_IP "$COMMAND"
+    if [ -z "$COMMAND" ]; then
+        ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_INSTANCE_IP" ubuntu@$PRIVATE_INSTANCE_IP
+    else
+        ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p ubuntu@$PUBLIC_INSTANCE_IP" ubuntu@$PRIVATE_INSTANCE_IP "$COMMAND"
+    fi
 }
 
 # Determine the action based on the number of arguments
@@ -40,11 +38,5 @@ if [ -z "$PRIVATE_INSTANCE_IP" ]; then
     connect_to_public_instance
 else
     # Private instance IP provided, connect to the private instance through the public instance
-    if [ -z "$COMMAND" ]; then
-        # No command provided, just connect to the private instance
-        connect_to_private_instance
-    else
-        # Command provided, execute the command on the private instance
-        execute_command_on_private_instance
-    fi
+    connect_to_private_instance
 fi
